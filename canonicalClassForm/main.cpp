3ab30907f9ddc38e8cc04438944ab4fc769623d5
noraAngelova <73758640+noraAngelova@users.noreply.github.com>
2023-03-15 18:04:52 +0200

    Update main.cpp

--- canonicalClassForm/main.cpp
+++ canonicalClassForm/main.cpp
@@ -2,22 +2,30 @@
 #include <iostream>
 
 int main() {
-  // Създава обект с конструктоп по подразбиране
-	Person p;
-	p.print();
-	std::cout << std::endl;
+    // Създава обект с конструктоп по подразбиране
+    Person p;
+    p.print();
+    std::cout << std::endl;
 	
-  // Създава обект с конструктор за копиране
-	Person p2 = p;
-	p2.print();
-	std::cout << std::endl;
+    // Създава обект с конструктор за копиране
+    Person p2 = p;
+    p2.print();
+    std::cout << std::endl;
+	
+	if (p == p2) {
+		std::cout << "It is the same person" << std::endl;
+	}
 
-  // Създава обект с конструктор с два параметъра
-	Person p3("Ivan", 23);
+    // Създава обект с конструктор с два параметъра
+    Person p3("Ivan", 23);
 
-  // Извършва присвояване между два създадени обекта чрез оператора за присвояване
-	p2 = p3;
-	p2.print();
+    // Извършва присвояване между два създадени обекта чрез оператора за присвояване
+    p2 = p3;
+    p2.print();
+	
+	if (p < p2) {
+		std::cout << "\n p is younger than p2";
+	}
 
-	return 0;
+    return 0;
 }
