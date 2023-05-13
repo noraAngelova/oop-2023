092af90dc92ac4438292357db33a0c298608e0b7
noraAngelova <73758640+noraAngelova@users.noreply.github.com>
2023-05-13 21:56:49 +0300

    Add more comments for student.hpp

--- inheritance/student.hpp
+++ inheritance/student.hpp
@@ -10,24 +10,30 @@ class Student : protected Person {
 protected:
 	char* fn;
 public:
+	// В класът има динамично заделяне на памет и е нужна реализация на канонично представяне
 	Student() {
 		fn = new char[1];
 		strcpy(fn, "\0");
 	}
+	// Конструктор с пареметри - трябва да инициализира собсвените и наследените член-данни
+	// Обръща се към конструкторите на базовите класове в инициализиращия си списък
+	// В инициализиращия списък следва да се инициализират и всички член-данни от тип дефиниран от нас клас, ако такива има
 	Student(char const* name, unsigned const& age, char const* fnStr) : Person(name, age) {
 		fn = new char[strlen(fnStr) + 1];
 		strcpy(fn, fnStr);
 	}
-	
+	// Деструктор - изтрива само собствените член-данни
 	~Student() {
 		delete[] fn;
 	}
-	
+
+	// Копиращ конструктор - има инициализиращ списък и извиква конструкторите на базовите класове и член-данните от тип дефиниран от нас клас.
 	Student(Student const& other) : Person(other) {
 		fn = new char[strlen(other.fn) + 1];
 		strcpy(fn, other.fn);
 	}
 
+	// Оператор за присвояване - НЯМА инициализиращ списък, инициализацията на наследените член-данни става чрез извикване на Person::operator= в тялото
 	Student& operator=(Student const& other) {
 		if (this != &other) {
 			Person::operator=(other);
