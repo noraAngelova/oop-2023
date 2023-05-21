dd71180021e6da6013657ef48a3efd4dadcdd1ca
noraAngelova <73758640+noraAngelova@users.noreply.github.com>
2023-05-21 19:56:49 +0300

    Create person.hpp

---
+++ inheritance/person.hpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#ifndef _PERSON_HPP
+#define _PERSON_HPP
+#include <iostream>
+
+class Person {
+protected:
+	// Използва се указател, тъй като името е с произволна дължина
+	// Ако в условието е посочена максимална дължина (256), може да се използва следната декларация char firstName[256] и не е необходимо динамично заделяне на памет
+	char* firstName;
+	unsigned age;
+	unsigned id;
+	static unsigned counter;
+
+	void copy(char const* name);
+public:
+	// Ако стойността по подразбиране на параметъра name е nullptr, трябва да се внимава с обработката и копирането на стойността
+	Person(char const* name = "\0", unsigned const& ageData = 0);
+	Person(Person const& other);
+	virtual ~Person();
+	Person& operator=(Person const& other);
+
+	// Move семантика
+	Person(Person&& other);
+	Person& operator=(Person&& other);
+
+	void print() const;
+
+	bool operator<(Person const& other) const;
+	bool operator==(Person const& other) const;
+
+	friend std::istream& operator>>(std::istream& is, Person& p);
+};
+
+#endif
