506a2998991de8280213c416acd018b7098eaf71
noraAngelova <73758640+noraAngelova@users.noreply.github.com>
2023-05-29 13:41:10 +0300

    Create singleton_v1.cpp

---
+++ architecture/singleton_v1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+// Версия 1 с използване на динамична памет и стандартна статично поле
+class System {
+  private:
+   System *instance;
+   unsigned generalData;
+ 
+   // Private constructor so that no objects can be created.
+   System(unsigned data) {
+      generalData = data;
+   }
+
+  public:
+   static System *getInstance() {
+      if (!instance) {
+        instance = new System(1); // Не се изтрива, дори да се направи конструктор, никъде не се вика delete
+      }
+      return instance;
+   }
+
+   int getData() {
+      return this -> generalData;
+   }
+
+   void setData(int data) {
+      this -> generalData = data;
+   }
+   
+   System(System const & other) = delete;
+   System& operator=(System const & other) = delete;
+   System() = delete;
+};
+
+// Инициализация на статичното поле, за да е различимо
+System *System::instance = nullptr;
+
+int main() {
+   System * client= client->getInstance();
+   std::cout << client->getData() << std::endl;
+   client->setData(100);
+   std::cout << client->getData() << std::endl;
+   return 0;
+}
